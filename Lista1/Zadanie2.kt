7d48b37314808663dd6108d17baebb0db26fcaa1
WiktoriaDomanska <wiktoria170504@gmail.com>
2026-03-04 19:56:07 +0100

    Rozwiązanie Lista 1 Zadanie 2

---
+++ Lista1/Zadanie2.kt
@@ -0,0 +1,16 @@
+fun isPalindrome(str: String): Boolean {
+    var start = 0
+    var end = str.length - 1
+    while (start < end) {
+        if (str[start] != str[end]) {
+            return false
+        }
+        start++
+        end--
+    }
+    return true
+}
+
+fun main() {
+    println(isPalindrome("abba"))
+}
